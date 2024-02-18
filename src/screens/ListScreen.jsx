bcf32a5ced4bb2e57c75f55a65d34359cb03a7c4
Linas <limax.workspace@gmail.com>
2024-02-18 11:37:53 +0100

    added

--- src/screens/ListScreen.jsx
+++ src/screens/ListScreen.jsx
@@ -79,10 +79,10 @@ const ListScreen = () => {
           </View>
         ) : (
           userLists.map((list) => (
-            <TouchableOpacity key={list._id} className='mt-2' onPress={() => handleNavigate(list._id)}>
-              <View className={`flex-row items-center justify-between w-full px-5 py-12 border-2 border-${list.colorTheme} rounded-xl`}>
+            <TouchableOpacity key={list._id} className={`mt-2`} onPress={() => handleNavigate(list._id)}>
+              <View className={`border-${list.colorTheme} flex-row items-center justify-between w-full px-5 py-12 border-2 rounded-xl`}>
                 <Text className='text-xl font-bold text-white'>{list.name}</Text>
-                <TouchableOpacity className={`items-center justify-center px-4 py-2 text-white bg-${list.colorTheme} border rounded-full`} onPress={() => handleNavigate(list._id)}>
+                <TouchableOpacity className={`items-center justify-center px-4 py-2 text-white bg-${list.colorTheme} rounded-full`} onPress={() => handleNavigate(list._id)}>
                   <ChevronRight color={'#fff'} size={20}/>
                 </TouchableOpacity>
               </View>
